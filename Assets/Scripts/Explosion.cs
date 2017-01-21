4d729b2b33a687c3285f9edf4a4ec28d2d359659
Windywyll <mauricejrd@gmail.com>
2017-01-21 10:27:49 +0100

    Explosions, local score manager and Musics

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+    public float ExplosionForce;
+    public int maxRebound = 3;
+    private int currentRebound;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (maxRebound != 0 && currentRebound >= maxRebound)
+            return;
+
+        Vector3 average = Vector3.zero;
+
+        for(int i = 0; i < col.contacts.Length; i++)
+        {
+            average += col.contacts[i].point;
+        }
+
+        average /= col.contacts.Length;
+
+        if(col.collider.tag != "Ground")
+            gameObject.GetComponent<Rigidbody>().AddExplosionForce(ExplosionForce * 100, average, 10.0f);
+
+        currentRebound++;
+    }
+
+    void Awake()
+    {
+        currentRebound = 0;
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public float score;
+    public List<float> listHighScore;
+    private int nbHighScore = 10;
+
+	// Use this for initialization
+	void Awake () {
+        score = 0;
+        listHighScore = new List<float>(nbHighScore + 1);
+        
+        for(int i = 0; i < nbHighScore; i++)
+        {
+            listHighScore[i] = PlayerPrefs.GetFloat("HighScore"+(i+1) , 0);
+        }
+	}
+
+    public void AddScore(float _points)
+    {
+        score += _points;
+    }
+
+    public void EndOfGame()
+    {
+        for(int i = nbHighScore - 1 ; i > 0; i--)
+        {
+            if(listHighScore[i] > score)
+            {
+                listHighScore.Insert(i + 1, score);
+            }
+        }
+
+        SaveScores();
+    }
+
+    private void SaveScores()
+    {
+        for(int i = 0; i < nbHighScore; i++)
+        {
+            PlayerPrefs.SetFloat("HighScore" + i, listHighScore[i]);
+        }
+    }
+}
