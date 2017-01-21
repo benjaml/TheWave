e74806f45e2c17c48d09594202b466c335b47c97
ETU\b.vidot <b.vidot@rubika-edu.com>
2017-01-21 18:25:48 +0100

    faille manager

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public AudioSource failleSound;
+    public AudioSource introSound;
+    public AudioSource gameLoopSound;
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene("Main");
+        gameLoopSound.Play();
+    }
+}
