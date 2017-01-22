990831958987905ec36f99adb72e450693971986
ETU\b.lefevre <benjaml>
2017-01-22 12:28:10 +0100

    Leaderboard working

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -43,16 +43,7 @@ public class ScoreManager : MonoBehaviour {
     private void Start()
     {
     }
-
-    // This function gets called when Authenticate completes
-    // Note that if the operation is successful, Social.localUser will contain data from the server.
-    private void ProcessAuthentication(bool success )
-    {
-        if (success)
-            Debug.Log("Authenticated");
-        else
-            Debug.Log("Failed to authenticate");
-    }
+    
 
     public void AddScore(float _points)
     {
@@ -74,63 +65,13 @@ public class ScoreManager : MonoBehaviour {
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-            SaveScores();
-        if (Input.GetKeyDown(KeyCode.Z))
-            DisplayLeaderboard();
-    }
 
-    private void DisplayLeaderboard()
-    {
-        leaderBoard.LoadScores(result =>
-        {
-            Debug.Log("Received " + leaderBoard.scores.Length + " scores");
-            foreach (IScore score in leaderBoard.scores)
-                Debug.Log(score);
-        });
     }
+    
 
     private void SaveScores()
     {
-        Social.ReportScore(Random.Range(0,10), LeaderBoardId, success =>
-        {
-            Debug.Log(success ? "Reported score successfully" : "Failed to report score");
-        });
-        /*for (int i = 0; i < nbHighScore; i++)
-        {
-            PlayerPrefs.SetFloat("HighScore" + i, listHighScore[i]);
-
-        }
-        Social.LoadScores(LeaderBoardId, scores =>
-        {
-            addScoreInLeaderBoard();
-        });*/
+        LeaderboardManager.instance.SubmitScore((long)score);
     }
-
-    private IScore[] addScoreInLeaderBoard(IScore lastScore, IScore[] highScores)
-    {
-        if (highScores.Length < 10)
-            if (lastScore.value < highScores[highScores.Length - 1].value)
-            {
-                highScores[highScores.Length] = lastScore;
-                return highScores;
-            }
-        int index = 0;
-        IScore[] ret = new IScore[10];
-        foreach(IScore score in highScores)
-        {
-            if (lastScore.value > score.value)
-            {
-                ret[index] = lastScore;
-            }
-            else
-            {
-                ret[index] = score;
-            }
-            index++;
-            if (index == 9)
-                return ret;
-        }
-        return ret;
-    }
+    
 }
